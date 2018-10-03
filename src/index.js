a1a2839628ea7ece326cc66beeb92b4d26efd108
vv3k <msg4vivek1987@gmail.com>
2018-10-03 23:28:38 +0530

    Starter Webpack Config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./scss/style.scss";
+
+console.log("Hello");
+let d = document.createElement("div");
+d.appendChild(document.createTextNode("Hello"));
+document.body.appendChild(d);
+console.log("Hello");
